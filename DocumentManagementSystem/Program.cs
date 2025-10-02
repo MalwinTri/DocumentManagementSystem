fd20571bca83358f6d02975ca5454f1952d67f3c
davidf <david@froeschl.com>
2025-10-02 08:55:27 +0200

    Enhance environment check for Swagger in Docker

--- DocumentManagementSystem/Program.cs
+++ DocumentManagementSystem/Program.cs
@@ -45,7 +45,7 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Docker")
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DMS v1"));
