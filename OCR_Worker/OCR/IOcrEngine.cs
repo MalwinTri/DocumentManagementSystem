df9900100bb464970a5a45394d6fc551e9ea1d52
MalwinTri <trimmel.m02@gmail.com>
2026-01-24 19:26:31 +0100

    chore: format IOcrEngine

--- OCR_Worker/OCR/IOcrEngine.cs
+++ OCR_Worker/OCR/IOcrEngine.cs
@@ -1,5 +1,6 @@
 ﻿namespace DocumentManagementSystem.OCR_Worker.OCR;
+
 public interface IOcrEngine
 {
-    Task<string> ExtractTextAsync(Stream pdf, CancellationToken ct = default);
+    Task<string> ExtractTextAsync(Stream pdfStream, CancellationToken ct);
 }
