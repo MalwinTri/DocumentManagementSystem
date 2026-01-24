5616dd2293a00325610076eaed07ad6aaf9890af
MalwinTri <trimmel.m02@gmail.com>
2026-01-24 20:49:34 +0100

    chore: fix whitespace in IOcrEngine

--- OCR_Worker/OCR/IOcrEngine.cs
+++ OCR_Worker/OCR/IOcrEngine.cs
@@ -1,6 +1,7 @@
-﻿namespace DocumentManagementSystem.OCR_Worker.OCR;
+namespace DocumentManagementSystem.OCR_Worker.OCR;
 
 public interface IOcrEngine
 {
     Task<string> ExtractTextAsync(Stream pdfStream, CancellationToken ct);
 }
+
